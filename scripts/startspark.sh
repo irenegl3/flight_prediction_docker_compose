ee8fc43dd8f7dea96578244d460c544028c1a3a7
IRENE <irene.garcialop@alumnos.upm.es>
2020-11-29 12:03:31 +0100

    version final

---
+++ scripts/startspark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#cargar models 
+rm -r models
+python3 resources/train_spark_mllib_model.py . &
+
+sleep 300
+
+# ejecutar master y worker de spark para prediccion
+./spark-2.4.4-bin-hadoop2.7/bin/spark-submit \
+  --class es.upm.dit.ging.predictor.MakePrediction \
+  --packages org.mongodb.spark:mongo-spark-connector_2.11:2.3.2,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0 \
+  /main/flight_prediction/target/scala-2.11/flight_prediction_2.11-0.1.jar &
+
+  sleep 123456789
---
+++ scripts/startwebserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 predict_flask.py &
+sleep 123456789
